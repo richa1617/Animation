b74602d6129551e49d0ac27357a0222003cf6ce8
Richa Mahajan <richamahajan1617@gmail.com>
2023-10-04 10:01:48 +0200

    Added basic animation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,13 @@
-
+import { motion } from "framer-motion";
 
 export default function Home() {
   return (
-   <div>
-    <h1>Hello world</h1>
-   </div>
-  )
+    <motion.div
+      initial={{ fontSize: "1rem" }}
+      animate={{ fontSize: "3rem", color: "blue" }}
+      transition={{ type: "spring", delay: 1 }}
+    >
+      <h1>Hello world</h1>
+    </motion.div>
+  );
 }
